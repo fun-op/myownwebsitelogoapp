d7caf6ee70280865815f0e40e32079bc3182dc50
fun-op <77846134+fun-op@users.noreply.github.com>
2021-03-26 21:43:32 +0530

    Update content_scripts.js

--- content_scripts.js
+++ content_scripts.js
@@ -1,1 +1,7 @@
-
+                var urll = window.location.href;
+if (urll.includes('https://www.blogger.com/comment.g?blogID=6525905139240806961&pageID=7559218020859183664')){
+     document.getElementById("publishBtn").click();
+}
+if (urll.includes('https://www.blogger.com/comment.g')){
+window.location.href='https://the---anirban.blogspot.com/p/swper-beta.html';
+}
